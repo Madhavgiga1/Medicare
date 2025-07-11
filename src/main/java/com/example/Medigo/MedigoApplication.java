a0592a45d7698b9fec641ede03da4b98ea6749d4
Madhav <sharmamadhav208@gmail.com>
2025-07-11 23:37:45 +0530

    Initial commit

---
+++ src/main/java/com/example/Medigo/MedigoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Medigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedigoApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(MedigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medigo/MedigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
